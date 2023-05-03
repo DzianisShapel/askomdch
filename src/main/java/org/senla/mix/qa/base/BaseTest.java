dd28482724f7815fb1780bd8e5e59808d8e6b12b
DzianisShapel <dzianis283@gmail.com>
2023-05-03 06:45:13 +0000

    fixes quitDriver

--- src/main/java/org/senla/mix/qa/base/BaseTest.java
+++ src/main/java/org/senla/mix/qa/base/BaseTest.java
@@ -1,22 +1,13 @@
 package org.senla.mix.qa.base;
 
 
-import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogEntry;
-import org.openqa.selenium.logging.LogType;
 import org.senla.mix.qa.factory.DriverManager;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 public class BaseTest {
     private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
@@ -30,8 +21,8 @@ public class BaseTest {
 
 
     @AfterMethod
-    public synchronized void quitDriver(String browser, ITestResult result) throws InterruptedException, IOException {
-        Thread.sleep(100);
+    public synchronized void quitDriver() throws InterruptedException, IOException {
+       /* Thread.sleep(100);
         LogEntries browserLogs = getDriver().manage().logs().get(LogType.BROWSER);
         List<LogEntry> allLogRows = browserLogs.getAll();
         if (allLogRows.size() > 0) {
@@ -42,7 +33,7 @@ public class BaseTest {
             }
             logWriter.flush();
             logWriter.close();
-        }
+        }*/
         getDriver().quit();
     }
 
